28ec36dc8957fc4079cd44d45ddfa47b4e341a4f
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-18 15:59:45 +0530

    demostret all Arithamatic Operation

---
+++ AssignmentQ6.java
@@ -0,0 +1,26 @@
+//Q6 write a java program demostret all Arithamatic Operation
+public class AssignmentQ6 {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 20;
+		
+		int Add = num1+num2;
+		int sub = num1-num2;
+		int mult = num1*num2;
+		int divide = num1/num2;
+		int mod = num1%num2;
+		
+		System.out.println("Addition of  num1 and num2 is =" +Add);
+		System.out.println("");
+		System.out.println("Substractio of  num1 and num2 is =" +sub);
+		System.out.println("");
+		System.out.println("Multiplication of  num1 and num2 is =" +mult);
+		System.out.println("");
+		System.out.println("Divition of  num1 and num2 is =" +divide);
+		System.out.println("");
+		System.out.println("Modulas of  num1 and num2 is =" +mod);
+
+	}
+
+}
