24b68e0883de2628c2734a957f948fa842a33298
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-18 15:55:52 +0530

    to find square and cube of given no

---
+++ AssignmentQ3.java
@@ -0,0 +1,18 @@
+//Write a program to calculate the square and cube of given no
+public class AssignmentQ3 {
+	
+	void Calculate(){
+		int a=10;
+		int Square = a*a;
+		int Cube = a*a*a;
+		System.out.println("The Square of Given No is ="+Square);
+		System.out.println(" ");
+		System.out.println("The Cube of Given No is ="+Cube);
+	}
+
+	public static void main(String[] args) {
+		AssignmentQ3 a2 = new AssignmentQ3();
+		a2.Calculate();
+	}
+
+}
