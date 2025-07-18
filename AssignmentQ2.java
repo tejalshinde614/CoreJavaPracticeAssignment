94aff0d4214b5b4f7e59f709875dc77e3be5f2da
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-18 15:54:42 +0530

    Reminder of two numbers

---
+++ AssignmentQ2.java
@@ -0,0 +1,13 @@
+//Q2 Write a Program to find the Reminder When one no is dividing by another
+//using Modulas(%)Operator
+public class AssignmentQ2 {
+
+	void Modulas( int a, int b){
+	int c=a%b;
+	System.out.println("The two no reminder is"+c);
+	}
+	public static void main(String[] args) {
+	AssignmentQ2 a1 = new AssignmentQ2();
+	a1.Modulas(100, 15);
+	}
+}
