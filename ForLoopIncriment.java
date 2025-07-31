bac9b0f6a11af1fe71ca25fbf9451ffcf4d420b7
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-31 21:19:14 +0530

    Add files via upload

---
+++ ForLoopIncriment.java
@@ -0,0 +1,14 @@
+package Package2;
+public class ForLoopIncriment {
+
+	public static void main(String[] args) {
+		//we want to print numbers from 1 to 5 usining for loop
+      //for(initilazatin; condisation; Updation)
+		
+		for(int i=1; i<=5; i++) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
