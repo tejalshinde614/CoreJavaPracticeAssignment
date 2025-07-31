f2c7b2bf1ce35881d559544befb9f13494e636c5
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-31 21:16:16 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+package Package2;
+public class ForLoop {
+
+	public static void main(String[] args) {
+		//we want to print numbers from 1 to 5 usining for loop
+      //for(initilazatin; condisation; Updation)
+		
+		for(int i = 5; i>=1; i--) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
