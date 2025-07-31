dba30c38b7c093063feee42924fe0df8b089aab8
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-31 21:19:42 +0530

    Even odd program

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+package Package2;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a numbe to check the no i Even or Odd");
+int num = sc.nextInt();
+
+if(num%2==0) {
+	System.out.println("numbe is Even");
+}
+else
+{
+	System.out.println("numbe is Odd");
+	
+		}
+	}
+
+}
