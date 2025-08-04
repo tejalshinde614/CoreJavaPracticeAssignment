48a1260b583f0d522b586f16ac8637026512badd
Shinde Tejal <tejalshinde614@gmail.com>
2025-08-04 21:29:48 +0530

    Add files via upload

---
+++ WhileStatement.java
@@ -0,0 +1,53 @@
+/* while() = it is also loop control statement which is used to itrate the statement multipal time
+util the condisationbecomes false
+syntax   : while(){
+                  }
+1) initilization
+2)condisation
+3)updesation
+
+for(init; cond; updation)
+{
+stmt;
+}
+
+
+init;
+ while(condition)
+  {
+  stmt;
+  updation; 
+  }
+  
+  ex for
+  int i;
+  for(int i=1;i<=5;i++){
+  syso(i);
+  }
+  
+  ex while
+  int i; 
+  while(i<=5)
+  {
+   syso(i);
+   i++
+  }
+  
+  
+  
+ */
+package Package2;
+
+public class WhileStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+while(i<=5) {
+
+	System.out.println(i);
+	i++;
+}
+	}
+
+}
