f2e5fbc55690d75d66b6e89d556c14a0ed749177
Shinde Tejal <tejalshinde614@gmail.com>
2025-08-20 09:09:02 +0530

    OddNumberPrint

---
+++ OddNumberPrint.java
@@ -0,0 +1,12 @@
+/*Print Odd Numbers from 1 to 100
+Print all even numbers from 2 to 100 using only a for loop.*/
+public class OddNumberPrint { 
+	
+	    public static void main(String[] args) {       
+	        	System.out.println("Print Even Numbers From 2 to 100");
+	        	for(int i = 1; i <= 100; i += 2) {
+	            System.out.println(i + " ");
+	        }
+	    }
+	}
+
