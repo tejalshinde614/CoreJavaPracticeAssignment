6d83ac9a94aa85f4568e05c5e020157d80941f3e
Shinde Tejal <tejalshinde614@gmail.com>
2025-08-20 09:08:25 +0530

    NumberPrint

---
+++ NumberPrint.java
@@ -0,0 +1,10 @@
+/*Print Numbers from 1 to 100
+Write a program that prints numbers from 1 to 100 using a for loop.*/
+
+public class NumberPrint {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(" "+ i);
+        }
+    }
+}
