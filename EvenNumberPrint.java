b57308bfede8e04895a60e2b7cb4e8786f33e139
Shinde Tejal <tejalshinde614@gmail.com>
2025-08-20 09:08:02 +0530

    EvenNumberPrint

---
+++ EvenNumberPrint.java
@@ -0,0 +1,12 @@
+/*Print Even Numbers from 2 to 100
+Print all even numbers from 2 to 100 using only a for loop.*/
+public class EvenNumberPrint { 
+	
+	    public static void main(String[] args) {       
+	        	System.out.println("Print Even Numbers From 2 to 100");
+	        	for(int i = 2; i <= 100; i += 2) {
+	            System.out.println(i + " ");
+	        }
+	    }
+	}
+
